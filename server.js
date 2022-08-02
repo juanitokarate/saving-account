74b29e641f28327601fe5a02e5ea398a475345e6
juangaribay222 <juangaribay222@gmail.com>
2022-08-03 03:56:04 +0800

    all-fine

---
+++ server.js
@@ -0,0 +1,18 @@
+const express   = require('express')
+const path      = require('path')
+const app       = express()
+
+app.use(
+    express.static(
+        __dirname + '/dist/saving-account-transaction/'
+    )
+)
+app.get(
+    '/*',
+    (req, res) => {
+        res.sendFile(
+            path.join(__dirname + '/dist/saving-account-transaction/')
+        )
+    } 
+)
+app.listen(process.env.PORT || 8080)
